834c3f7a6214e22a7210a47a712ebb47ce57350d
Victor Dhedin <victor.dhedin@tum.de>
2025-03-27 13:42:24 +0000

    Move box closer to the robot.

--- scene/climb_box.py
+++ scene/climb_box.py
@@ -7,13 +7,13 @@ from scene.utils import vis_surfaces_normal
 def setup_scene(sim : Simulator, height : float, edge : float, vis_normal : bool = False) -> List[Surface]:
 
     ################## Box
-    offset = 0.4
+    offset = 0.35
 
     pos =   [offset + edge/2., 0., height/2.]
     size =  [edge/2., edge/1.5, height/2.]
     euler = [0., 0., 0.]
     box = Box(pos, size, euler)
-    plane = Surface(np.array([0., 0., 0.]), np.array([0., 0., 1.]), rot=np.eye(3), size_x=1e10, size_y=1e10)
+    plane = Surface(np.array([0., 0., 0.]), np.array([0., 0., 1.]), rot=np.eye(3), size_x=1e1, size_y=1e1)
 
     all_surfaces = [plane] + box.get_surfaces()
     # floor, top, side
