66c26ee77182505340a7de56bd7655c0ef8c5ba1
Victor Dhedin <victor.dhedin@tum.de>
2025-05-05 15:28:02 +0000

    Add cached data.

--- search/save_data.py
+++ search/save_data.py
@@ -8,26 +8,28 @@ class DataSaver:
     def __init__(self, save_dir):
         self.save_dir = save_dir
         self.datafile_path = os.path.join(self.save_dir, FILE_NAME)
-            
-    def append(self, **kwargs):
-        
-        data = self.load()
-        if data is None:
-            data = {}
-            
-        for k, v in kwargs.items():
-            if k in data:
-                data[k].append(v)
+        self._data = None  # Lazy-loaded cache
+
+    def _ensure_data_loaded(self):
+        if self._data is None:
+            if os.path.exists(self.datafile_path):
+                with open(self.datafile_path, 'r') as file:
+                    self._data = yaml.safe_load(file) or {}
             else:
-                data[k] = [v]
+                self._data = {}
+
+    def append(self, **kwargs):
+        self._ensure_data_loaded()
+
+        for k, v in kwargs.items():
+            if k in self._data:
+                self._data[k].append(v)
+            else:
+                self._data[k] = [v]
 
         with open(self.datafile_path, 'w') as file:
-            yaml.safe_dump(data, file, default_flow_style=False)
-            
+            yaml.safe_dump(self._data, file, default_flow_style=False)
+
     def load(self):
-        if not os.path.exists(self.datafile_path):
-            return None
-        
-        with open(self.datafile_path, 'r') as file:
-            data = yaml.safe_load(file)
-        return data
+        self._ensure_data_loaded()
+        return self._data
