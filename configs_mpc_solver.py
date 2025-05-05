80d46a8c5674189317f861e3985212617ec733ec
Victor Dhedin <victor.dhedin@tum.de>
2025-05-05 12:22:57 +0000

    Move configs to new file. Add tuning script.

---
+++ configs_mpc_solver.py
@@ -0,0 +1,171 @@
+import numpy as np
+import os
+from typing import Any, List
+
+from mj_pin.utils import get_robot_description
+from mpc_controller.config.config_abstract import MPCOptConfig, MPCCostConfig, GaitConfig, HPIPM_MODE
+from mpc_controller.mpc_acyclic import AcyclicMPC
+
+FILE_NAME = os.path.split(__file__)[1]
+
+BASE_SAVE_DIR = "data"
+# SIM
+ROBOT_NAME = "go2"
+SIM_DT = 1/500
+# CLOSE LOOP MPC
+DT = 0.035
+N_NODES_MPC = 50
+# TRAJ OPT
+RECOMPILE = False
+N_NODES_SOLVER = 50
+DURATION = 3 # Traj opt with a coarser discretization
+MAX_IT = 75
+MAX_QP = 7
+
+##################  Solver
+# Opt
+config_solver = MPCOptConfig(
+    time_horizon=DURATION,
+    n_nodes=N_NODES_SOLVER,
+    replanning_freq=1, Kp=1, Kd=1,
+    recompile=RECOMPILE,
+    max_iter=MAX_IT,
+    max_qp_iter=7,
+    opt_peak=True,
+    warm_start_sol=False,
+    nlp_tol=1.0e-2,
+    qp_tol=1.0e-3,
+    hpipm_mode=HPIPM_MODE.balance,
+    torque_limit=True,
+    mu=0.7,
+    solver_name="traj_opt_solver"
+)
+
+config_close_loop = MPCOptConfig(
+    time_horizon=N_NODES_MPC * DT,
+    n_nodes=N_NODES_MPC,
+    replanning_freq=15,
+    Kp=20.,
+    Kd=2.,
+    recompile=RECOMPILE,
+    max_iter=MAX_IT,
+    max_qp_iter=MAX_QP,
+    opt_peak=True,
+    warm_start_sol=True,
+    nlp_tol=1.0e-2,
+    qp_tol=1.0e-3,
+    hpipm_mode=HPIPM_MODE.speed,
+    solver_name="mpc_solver"
+)
+
+
+# Cost
+def __init_np(l : List, scale : float=1.):
+    """ Init numpy array field."""
+    return np.array(l) * scale
+
+W = [
+        0e0, 0e0, 0e0,      # Bas5e position weights
+        5e0, 1e1, 3e1,      # Base orientation (ypr) weights
+        1e0, 1e0, 1e-1,      # Base linear velocity weights
+        5e0, 5e0, 5e0,      # Base angular velocity weights
+    ]
+
+W_e = [
+        0e0, 0e0, 0e0,      # Base position weights
+        1e0, 1e0, 1e0,      # Base orientation (ypr) weights
+        1e1, 1e1, 1e1,      # Base linear velocity weights
+        1e1, 1e1, 1e1,      # Base angular velocity weights
+    ]
+
+n_feet = 4
+HSE_SCALE = [3., 2., 1.] *  n_feet
+
+config_cost = MPCCostConfig(
+    robot_name=ROBOT_NAME,
+    gait_name="",
+    W_e_base=__init_np(W_e, 50.),
+    W_base=__init_np(W, 25.),
+    W_joint=__init_np(HSE_SCALE + [0.3] * len(HSE_SCALE), 50.),
+    W_e_joint=__init_np(HSE_SCALE + [1.] * len(HSE_SCALE), 50.),
+    W_acc=__init_np(HSE_SCALE, 1.e-2),
+    W_swing=__init_np([1e3] * n_feet),
+    W_eeff_ori=__init_np([1e2] * n_feet),
+    W_cnt_f_reg = __init_np([[0.1, 0.1, 0.1]] * n_feet),
+    W_foot_pos_constr_stab = __init_np([1e2] * n_feet),
+    W_foot_displacement = __init_np([0.]),
+    cnt_radius = 0.015, # m
+    time_opt = __init_np([0.0e4]),
+    W_swing_v = 300.,
+    W_below_plane = 2000.,
+    W_plane_border = 500.,
+    reg_eps = 1.0e-6,
+    reg_eps_e = 1.0e-5,
+)
+
+config_cost_close_loop = MPCCostConfig(
+    robot_name=ROBOT_NAME,
+    gait_name="",
+    W_e_base=__init_np(W_e, 10.),
+    W_base=__init_np(W, 25.),
+    W_joint=__init_np(HSE_SCALE + [0.5] * len(HSE_SCALE), 15.),
+    W_e_joint=__init_np(HSE_SCALE + [0.1] * len(HSE_SCALE), 10.),
+    W_acc=__init_np(HSE_SCALE, 1.e-2),
+    W_swing=__init_np([1e4] * n_feet),
+    W_eeff_ori=__init_np([1e2] * n_feet),
+    W_cnt_f_reg = __init_np([[0.1, 0.1, 0.1]] * n_feet),
+    W_foot_pos_constr_stab = __init_np([5e1] * n_feet),
+    W_foot_displacement = __init_np([0.]),
+    cnt_radius = 0.015, # m
+    time_opt = __init_np([0.0e4]),
+    W_swing_v = 300.,
+    W_below_plane = 2000.,
+    W_plane_border = 500.,
+    reg_eps = 1.0e-6,
+    reg_eps_e = 1.0e-5,
+)
+
+config_gait = GaitConfig(
+    "acyclic",
+    DURATION,
+    np.array([0.1, 0.1, 0.1, 0.1]),
+    np.array([0.1, 0.1, 0.1, 0.1]),
+    0.3,
+    0.06,
+)
+
+
+### INIT
+robot_description = get_robot_description(ROBOT_NAME)
+mj_feet_frames = ["FL", "FR", "RL", "RR"]
+pin_feet_frames = [f + "_foot" for f in mj_feet_frames]
+
+mpc_solver = AcyclicMPC(
+    robot_description.urdf_path,
+    pin_feet_frames,
+    config_solver,
+    config_cost,
+    config_gait,
+    joint_ref=robot_description.q0,
+    sim_dt=SIM_DT,
+    height_offset=0.,
+    print_info=False,
+    compute_timings=False,
+    solve_async=False,
+)
+mpc_solver.config_opt.recompile = False
+
+mpc_close_loop = AcyclicMPC(
+    robot_description.urdf_path,
+    pin_feet_frames,
+    config_close_loop,
+    config_cost_close_loop,
+    config_gait,
+    joint_ref=robot_description.q0,
+    sim_dt=SIM_DT,
+    height_offset=0.,
+    print_info=False,
+    compute_timings=False,
+    solve_async=False,
+)
+mpc_close_loop.config_opt.recompile = False
---
+++ test_traj_opt.py
@@ -0,0 +1,96 @@
+import numpy as np
+from typing import Any, List
+import time
+import os
+
+from mj_pin.simulator import Simulator
+from mpc_controller.config.config_abstract import MPCOptConfig, MPCCostConfig, GaitConfig, HPIPM_MODE
+from search.mcts_locomotion_task import MCTSPhaseLocomotionTask
+from configs_mpc_solver import *
+
+# from scene.climb_box import setup_scene, SCENE_NAME
+# HEIGHT = 0.2
+# EDGE = 0.4
+# OFFSET = 0.5
+
+# sim = Simulator(robot_description.xml_scene_path, sim_dt=SIM_DT)
+# surfaces = setup_scene(sim, height=HEIGHT, edge=EDGE, offset=OFFSET, save_dir="", vis_normal=False)
+# if HEIGHT <= 0.3:
+#     surfaces = surfaces[:2]
+
+# phase_sequence = [
+#     (0, (1, 1, 1, 1), (0, 0, 0, 0)),
+#     (0, (0, 1, 1, 1), (0, 0, 0)),
+#     (0, (1, 0, 1, 1), (1, 0, 0)),
+#     (0, (1, 1, 1, 0), (1, 1, 0)),
+#     (0, (1, 1, 0, 1), (1, 1, 0)),
+#     (0, (1, 0, 1, 1), (1, 0, 0)),
+#     (0, (0, 1, 1, 1), (1, 0, 0)),
+#     (0, (1, 1, 1, 1), (1, 1, 0, 0)),
+#     # (0, (1, 1, 1, 1), (1, 1, 0, 0)),
+#     (0, (1, 1, 1, 0), (1, 1, 0)),
+#     (0, (1, 1, 0, 1), (1, 1, 1)),
+#     (0, (1, 1, 1, 1), (1, 1, 1, 1)),
+# ]
+
+    
+from scene.cross_gap import setup_scene, SCENE_NAME
+GAP_LENGTH = 0.4  # Adjustable gap between start and goal
+WALL_ANGLE = np.radians(65)  # Adjustable wall angle
+h_offset = 0.1
+
+sim = Simulator(robot_description.xml_scene_path, sim_dt=SIM_DT)
+surfaces = setup_scene(sim, gap_length=GAP_LENGTH, wall_angle=WALL_ANGLE, height=h_offset/2, save_dir="",)
+q0 = robot_description.q0
+q0[2] += h_offset
+sim.set_initial_state(q0)
+
+
+phase_sequence = [
+    (0, (1, 1, 1, 1), (0, 0, 0, 0)),
+    (0, (1, 1, 1, 1), (0, 0, 0, 0)),
+    (1, (0, 0, 1, 1), (0, 0)),
+    (2, (1, 1, 1, 1), (2, 3, 0, 0)),
+    (1, (1, 1, 1, 0), (2, 3, 0)),
+    (1, (1, 1, 0, 1), (2, 3, 0)),
+    (1, (1, 1, 0, 0), (2, 3,)),
+    (2, (1, 1, 1, 1), (2, 3, 2, 3)),
+    (3, (0, 1, 1, 1), (3, 2, 3)),
+    (1, (1, 0, 1, 1), (1, 2, 3)),
+    (1, (1, 1, 0, 1), (1, 1, 3)),
+    (1, (1, 1, 1, 0), (1, 1, 1)),
+    (6, (1, 1, 1, 1), (1, 1, 1, 1))
+    ]
+    
+if __name__ == "__main__":
+    
+    ITERATIONS = 2000
+    C = 1.5
+    ALPHA = 0.5
+    N_PHASES = 12
+    GOAL = (1, 1, 1, 1)
+    START_NODE = (0, (1, 1, 1, 1), (0, 0, 0, 0))
+    MIN_IN_CNT = 2
+    MIN_RES = 0.
+    MIN_AVG_COLLISION = 0.
+    
+    # MCTS search 
+    mcts = MCTSPhaseLocomotionTask(
+        C=C,
+        alpha_exploration=ALPHA,
+        sim=sim,
+        mpc_solver=mpc_solver,
+        mpc_close_loop=mpc_close_loop,
+        n_phases=N_PHASES,
+        surfaces=surfaces,
+        min_in_cnt=MIN_IN_CNT,
+        goal_surf_id=GOAL,
+        min_mpc_log10_prod_res=MIN_RES,
+        min_mpc_avg_collision=MIN_AVG_COLLISION,
+        save_dir=""
+        )
+
+    q_sol, v_sol, dt_sol = mcts.run_traj_opt(phase_sequence)
+    q_mj_traj = np.stack([mcts.mpc_solver.solver.dyn.convert_to_mujoco(q, v)[0] for q, v in zip(q_sol, v_sol)])
+    time_traj = np.concatenate(([0.], np.cumsum(dt_sol)))
+    mcts.sim.visualize_trajectory(q_mj_traj, time_traj, record_video=False)
